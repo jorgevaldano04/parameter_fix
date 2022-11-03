92f6d08afa05ad25665173d046b216cbf7129115
jorge valdano <jorgevaldano@student.telkomuniversity.ac.id>
2022-11-03 14:09:56 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+ <h2 class="mb-4" style="color:#008080;">Daftar Parameter</h2>
+   <table class="table table-striped" id="example">
+    <thead class="thead-light">
+      <tr class="table-primary">
+        <th scope="col">No</th>
+         <th scope="col">Nama Parameter</th>
+         <th scope="col">Value</th>  
+         <th scope="col">Aksi</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(user, index) in users" :key="user.id">
+        <td>{{ index + 1 }}</td>
+        <td>{{user.name}}</td>
+        <td>{{user.username}}</td>
+        <td><ButtonDetail :visible="false" variant="success">email: {{ user.email }}</ButtonDetail></td>
+    
+      </tr>
+      
+    </tbody>
+  </table>
+</div>
+  
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//Bootstrap and jQuery libraries
+import 'bootstrap/dist/css/bootstrap.min.css'; //for table good looks
+import 'jquery/dist/jquery.min.js';
+//Datatable Modules
+import "datatables.net-dt/js/dataTables.dataTables"
+import "datatables.net-dt/css/jquery.dataTables.min.css"
+import "datatables.net-buttons/js/dataTables.buttons.js"
+import "datatables.net-buttons/js/buttons.colVis.js"
+import $ from 'jquery'; 
+import axios from 'axios'; //for api calling
+import ButtonDetail from '@/components/ButtonDetail.vue'
 export default {
-  name: 'App',
+  name:"App",
+  data(){
+    return{
+      users:[]
+    };
+  },
+ 
+  mounted(){
+    //Web api calling for dynamic data and you can also use into your demo project
+    axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((res)=>
+    {
+      this.users = res.data;
+      setTimeout(function(){
+      $('#example').DataTable(
+          {
+              pagingType: 'full_numbers',
+                pageLength: 5,
+                processing: true,
+          }
+      );
+      },
+        );
+    })
+  },
   components: {
-    HelloWorld
-  }
+    ButtonDetail
+},
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  max-width: 70rem;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding: 2rem;
+  border: 2px solid #ccc;
+  border-radius: 12px;
 }
+
 </style>
